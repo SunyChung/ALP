ff01f61e4033ed80040005ded92068e8cda60ac3
sy-chung <38775109+sy-chung@users.noreply.github.com>
2022-12-02 21:05:59 +0900

    Update train_eval.py

--- train_eval.py
+++ train_eval.py
@@ -14,7 +14,6 @@ import matplotlib
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-from graph_utils import PyGGraph_to_nx
 from torch_geometric.data import DataListLoader
 
 
@@ -44,8 +43,8 @@ def train_multiple_epochs(train_dataset,
         num_workers = 2
     # 여기서 batch 설정하려면 ?? -> follow_batch
     train_loader = DataLoader(train_dataset, batch_size, shuffle=False,  # shuffle=True,
-                              # follow_batch=['x', 'edge_index', 'y'],
-                              follow_batch=['x'],
+                              follow_batch=['x', 'edge_index', 'y'],
+                              # follow_batch=['x'],
                               num_workers=num_workers)
 
     if test_dataset.__class__.__name__ == 'MyDynamicDataset':
@@ -53,8 +52,8 @@ def train_multiple_epochs(train_dataset,
     else:
         num_workers = 2
     test_loader = DataLoader(test_dataset, batch_size, shuffle=False,
-                             # follow_batch=['x', 'edge_index', 'y'],
-                             follow_batch=['x'],
+                             follow_batch=['x', 'edge_index', 'y'],
+                             # follow_batch=['x'],
                              num_workers=num_workers)
 
     model.to(device).reset_parameters()
@@ -134,10 +133,7 @@ def test_once(test_dataset,
 
     model.to(device)
     t_start = time.perf_counter()
-    if ensemble and checkpoints:
-        rmse = eval_rmse_ensemble(model, checkpoints, test_loader, device, show_progress=True)
-    else:
-        rmse = eval_rmse(model, test_loader, device, show_progress=True)
+    rmse = eval_rmse(model, test_loader, device, show_progress=True)
 
     t_end = time.perf_counter()
     duration = t_end - t_start
@@ -173,6 +169,8 @@ def train(model, optimizer, loader, device, regression=False,
         optimizer.zero_grad()
         data = data.to(device)
         out = model(data)
+        # print('out : ', out.shape)  # torch.Size([50, 71])
+        # print('data.y : ', data.y.shape)  # torch.Size([50])
         # print('out : ', out)
         # print('data.y : ', data.y)
         if regression:
@@ -201,6 +199,7 @@ def train(model, optimizer, loader, device, regression=False,
 def eval_loss(model, loader, device, regression=False, show_progress=False):
     model.eval()
     loss = 0
+    count = 0
     if show_progress:
         print('Testing begins...')
         pbar = tqdm(loader)
@@ -210,10 +209,17 @@ def eval_loss(model, loader, device, regression=False, show_progress=False):
         data = data.to(device)
         with torch.no_grad():
             out = model(data)
+        # print('out : ', out)
+        # print('data.y : ', data.y)
         if regression:
             loss += F.mse_loss(out, data.y.view(-1), reduction='sum').item()
         else:
-            loss += F.nll_loss(out, data.y.view(-1), reduction='sum').item()
+            eval_l = F.nll_loss(out, data.y.view(-1), reduction='sum').item()
+            count += 1
+            # print('count : ', count)
+            print('per loss : ', eval_l)
+            loss += eval_l
+            print('loss : ', loss)
         torch.cuda.empty_cache()
     return loss / len(loader.dataset)
 
@@ -222,121 +228,3 @@ def eval_rmse(model, loader, device, regression, show_progress=False):
     mse_loss = eval_loss(model, loader, device, regression, show_progress)
     rmse = math.sqrt(mse_loss)
     return rmse
-
-
-def eval_loss_ensemble(model, checkpoints, loader, device, regression=False,
-                       show_progress=False):
-    loss = 0
-    Outs = []
-    for i, checkpoint in enumerate(checkpoints):
-        if show_progress:
-            print('Testing begins...')
-            pbar = tqdm(loader)
-        else:
-            pbar = loader
-        model.load_state_dict(torch.load(checkpoint))
-        model.eval()
-        outs = []
-        if i == 0:
-            ys = []
-        for data in pbar:
-            data = data[0].to(device)
-            if i == 0:
-                ys.append(data.y.view(-1))
-            with torch.no_grad():
-                out = model(data)
-                outs.append(out)
-        if i == 0:
-            ys = torch.cat(ys, 0)
-        outs = torch.cat(outs, 0).view(-1, 1)
-        Outs.append(outs)
-    Outs = torch.cat(Outs, 1).mean(1)
-    if regression:
-        loss += F.mse_loss(Outs, ys, reduction='sum').item()
-    else:
-        loss += F.nll_loss(Outs, ys, reduction='sum').item()
-    torch.cuda.empty_cache()
-    return loss / len(loader.dataset)
-
-
-def eval_rmse_ensemble(model, checkpoints, loader, device, show_progress=False):
-    mse_loss = eval_loss_ensemble(model, checkpoints, loader, device, True, show_progress)
-    rmse = math.sqrt(mse_loss)
-    return rmse
-
-
-def visualize(model, graphs, res_dir, data_name, class_values, num=5, sort_by='prediction'):
-    model.eval()
-    model.to(device)
-    R = []
-    Y = []
-    graph_loader = DataLoader(graphs, 50, shuffle=False)
-    for data in tqdm(graph_loader):
-        data = data[0].to(device)
-        r = model(data).detach()
-        y = data.y
-        R.extend(r.view(-1).tolist())
-        Y.extend(y.view(-1).tolist())
-    if sort_by == 'true':  # sort graphs by their true ratings
-        order = np.argsort(Y).tolist()
-    elif sort_by == 'prediction':
-        order = np.argsort(R).tolist()
-    elif sort_by == 'random':  # randomly select graphs to visualize
-        order = np.random.permutation(range(len(R))).tolist()
-    highest = [PyGGraph_to_nx(graphs[i]) for i in order[-num:][::-1]]
-    lowest = [PyGGraph_to_nx(graphs[i]) for i in order[:num]]
-    highest_scores = [R[i] for i in order[-num:][::-1]]
-    lowest_scores = [R[i] for i in order[:num]]
-    highest_ys = [Y[i] for i in order[-num:][::-1]]
-    lowest_ys = [Y[i] for i in order[:num]]
-    scores = highest_scores + lowest_scores
-    ys = highest_ys + lowest_ys
-    type_to_label = {0: 'u0', 1: 'v0', 2: 'u1', 3: 'v1', 4: 'u2', 5: 'v2'}
-    type_to_color = {0: 'xkcd:red', 1: 'xkcd:blue', 2: 'xkcd:orange',
-                     3: 'xkcd:lightblue', 4: 'y', 5: 'g'}
-    plt.axis('off')
-    f = plt.figure(figsize=(20, 10))
-    axs = f.subplots(2, num)
-    cmap = plt.cm.get_cmap('rainbow')
-    vmin, vmax = min(class_values), max(class_values)
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    sm.set_array([])
-    for i, g in enumerate(highest + lowest):
-        u_nodes = [x for x, y in g.nodes(data=True) if y['type'] % 2 == 0]
-        u0, v0 = 0, len(u_nodes)
-        pos = nx.drawing.layout.bipartite_layout(g, u_nodes)
-        bottom_u_node = min(pos, key=lambda x: (pos[x][0], pos[x][1]))
-        bottom_v_node = min(pos, key=lambda x: (-pos[x][0], pos[x][1]))
-        # swap u0 and v0 with bottom nodes if they are not already
-        if u0 != bottom_u_node:
-            pos[u0], pos[bottom_u_node] = pos[bottom_u_node], pos[u0]
-        if v0 != bottom_v_node:
-            pos[v0], pos[bottom_v_node] = pos[bottom_v_node], pos[v0]
-        labels = {x: type_to_label[y] for x, y in nx.get_node_attributes(g, 'type').items()}
-        node_colors = [type_to_color[y] for x, y in nx.get_node_attributes(g, 'type').items()]
-        edge_types = nx.get_edge_attributes(g, 'type')
-        edge_types = [class_values[edge_types[x]] for x in g.edges()]
-        axs[i // num, i % num].axis('off')
-        nx.draw_networkx(g, pos,
-                         # labels=labels,
-                         with_labels=False,
-                         node_size=150,
-                         node_color=node_colors, edge_color=edge_types,
-                         ax=axs[i // num, i % num], edge_cmap=cmap, edge_vmin=vmin, edge_vmax=vmax,
-                         )
-        # make u0 v0 on top of other nodes
-        nx.draw_networkx_nodes(g, {u0: pos[u0]}, nodelist=[u0], node_size=150,
-                               node_color='xkcd:red', ax=axs[i // num, i % num])
-        nx.draw_networkx_nodes(g, {v0: pos[v0]}, nodelist=[v0], node_size=150,
-                               node_color='xkcd:blue', ax=axs[i // num, i % num])
-        axs[i // num, i % num].set_title('{:.4f} ({:})'.format(
-            scores[i], ys[i]), x=0.5, y=-0.05, fontsize=20
-        )
-    f.subplots_adjust(right=0.85)
-    cbar_ax = f.add_axes([0.88, 0.15, 0.02, 0.7])
-    if len(class_values) > 20:
-        class_values = np.linspace(min(class_values), max(class_values), 20, dtype=int).tolist()
-    cbar = plt.colorbar(sm, cax=cbar_ax, ticks=class_values)
-    cbar.ax.tick_params(labelsize=22)
-    f.savefig(os.path.join(res_dir, "visualization_{}_{}.pdf".format(data_name, sort_by)),
-              interpolation='nearest', bbox_inches='tight')
